da71ef12625994157c35f54113f11458eed0e665
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-02-04 23:34:19 +0530

    Added Customer Class

---
+++ SharedProjectSample/CustomerModel/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerModel
+{
+    public class Customer
+    {
+		public int CustomerId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		
+	}
+}
