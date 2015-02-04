a09f14538ab249d656d88f3491b6fa619eba5ac1
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2015-02-04 23:51:54 +0530

    Added Console application

---
+++ SharedProjectSample/TestConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CustomerModel;
+using System;
+
+namespace TestConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Customer customer = new Customer
+			{
+				CustomerId = 1,
+				FirstName = "Jalpesh",
+				LastName = "Vadgama"
+			};
+
+			Console.WriteLine("Firstname : {0}",customer.FirstName);
+			Console.WriteLine("Lastname : {0}", customer.LastName);
+
+		}
+	}
+}
